4f0afeece21a3157288820b9e5ce5b0d8b083165
Simone Giannuario <simone.giannuario@external.eni.com>
2025-09-02 20:10:51 +0200

    fix: fixed logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Logo: React.FC = () => {
   return (
-    <Link to="/" className="header-logo" aria-label="Go to homepage">
+    <Link to="/" className="header-logo logo" aria-label="Go to homepage">
       <div className="logo-container">
         <div className="logo-circle-small"></div>
         <span className="logo-text-small">SG</span>
